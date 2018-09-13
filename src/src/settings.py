6b5f0fa35034f0f97d877c316f836fa8157cdf7a
Eivind K. Dovik <eivind.doe@gmail.com>
2018-09-13 09:06:52 +0200

    collect static

--- src/src/settings.py
+++ src/src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i9%1nc@jxasu)rivx$dx62%e*b=@02481u#y&+@eur*e+#0sbx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['delelager.fodo.io']
+ALLOWED_HOSTS = ['delelager.fodo.io', '127.0.0.1']
 
 
 # Application definition
@@ -124,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
